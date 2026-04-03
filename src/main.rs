92849b0f12c4f0735aa069f90b197cf88dac9e74
Takuya Saito <takus4649@gmail.com>
2026-04-04 00:42:06 +0900

    Implement column-zigzag baseline solver

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use proconio::input;
+﻿use proconio::input;
 use std::time::Instant;
-use rand::{rngs::StdRng, Rng, SeedableRng};
+use rand::{rngs::StdRng, SeedableRng};
 
 fn read_input() -> Input {
     input! {
@@ -27,6 +27,7 @@ struct Solver {
     rng: StdRng,
     start: Instant,
     input: Input,
+    ops: Vec<char>,
 }
 
 impl Solver {
@@ -37,15 +38,27 @@ impl Solver {
             rng,
             start,
             input,
+            ops: Vec::new(),
         }
     }
 
     fn solve(&mut self) {
-        // ここに解法を実装
+        let path = self.build_zigzag_path();
+        let start_index = path
+            .iter()
+            .position(|&cell| cell == (4, 0))
+            .expect("initial head must be on the path");
+
+        self.ops = path[start_index..]
+            .windows(2)
+            .map(|cells| Self::step(cells[0], cells[1]))
+            .collect();
     }
 
     fn ans(&self) {
-        // ここに解答の出力を実装
+        for &op in &self.ops {
+            println!("{op}");
+        }
     }
 
     fn score(&self) -> usize {
@@ -54,6 +67,37 @@ impl Solver {
 
     fn result(&self) {
         eprintln!("{{ \"score\": {} }}", self.score());
+    }
+
+    fn build_zigzag_path(&self) -> Vec<(usize, usize)> {
+        let mut path = Vec::with_capacity(self.input.n * self.input.n);
+
+        for j in 0..self.input.n {
+            if j % 2 == 0 {
+                for i in 0..self.input.n {
+                    path.push((i, j));
+                }
+            } else {
+                for i in (0..self.input.n).rev() {
+                    path.push((i, j));
+                }
+            }
+        }
+
+        path
+    }
+
+    fn step(from: (usize, usize), to: (usize, usize)) -> char {
+        match (
+            to.0 as isize - from.0 as isize,
+            to.1 as isize - from.1 as isize,
+        ) {
+            (-1, 0) => 'U',
+            (1, 0) => 'D',
+            (0, -1) => 'L',
+            (0, 1) => 'R',
+            _ => panic!("path must move to an adjacent cell"),
+        }
     }
 }
 
